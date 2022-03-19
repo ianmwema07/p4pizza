f3398e374b635dc380fa09513ff16f9d093615ee
iano <ianmwema07@gmail.com>
2022-03-19 23:11:31 +0300

    add the business logic for calculations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,10 +24,13 @@ const sizePrices = {
   large : 900
 }
 
+
+
+var pricesArray
 const crustPrices = {
   neapolitanPizza : 120,
-  newYourkStylePizz : 240,
-  stLouise : 360
+  newYorkStylePizza : 240,
+  stLouisePizza : 360
 }
 
 const toppingsPrices = {
@@ -35,6 +38,18 @@ const toppingsPrices = {
   sausage : 200,
   bacon : 300
 }
+
+var pricesArray = [
+  parseInt(sizePrices.small),
+  parseInt(sizePrices.medium),
+  parseInt(sizePrices.large),
+  parseInt(crustPrices.neapolitanPizza),
+  parseInt(crustPrices.newYorkStylePizza),
+  parseInt(crustPrices.stLouisePizza),
+  parseInt(toppingsPrices.onions),
+  parseInt(toppingsPrices.sausage),
+  parseInt(toppingsPrices.bacon),
+];
 
 /*Getting data from the input form*/
 $(document).ready(function(){
@@ -50,9 +65,99 @@ $(document).ready(function(){
    
 
     var newOrder = new Order(selectedCrust,selectedToppings,selectedSize,selectedAmount);
-    var newDelivery = new Delivery(selectedDelivery,deliveryLocation);
+    // var newDelivery = new Delivery(selectedDelivery,deliveryLocation);
+
+    if( newOrder.size == "Small"){
+      if ( newOrder.crust == "Neapolitan Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[0] + pricesArray[3] +pricesArray[6];
+        }else if(newOrder.toppings == "Sausage"){
+          var totalPrice = pricesArray[0] + pricesArray[3] +pricesArray[7];
+        }else if(newOrder.toppings == "Bacon"){
+          var totalPrice = pricesArray[0] + pricesArray[3] +pricesArray[8];
+        }
+      }else if(newOrder.crust == "New York Style Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[0] + pricesArray[4] +pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[0] + pricesArray[4] +pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[0] + pricesArray[4] +pricesArray[8];
+        }
+      }else if(newOrder.crust == "St. Louis Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[0] + pricesArray[5]  +pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[0] + pricesArray[5]  +pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[0] + pricesArray[5]  +pricesArray[8];
+        }
+      }
+    }else if( newOrder.size == "Medium"){
+      if ( newOrder.crust == "Neapolitan Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[1] + pricesArray[3] +pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[1] + pricesArray[3] +pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[1] + pricesArray[3] +pricesArray[8];
+        }
+      }else if(newOrder.crust == "New York Style Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[1] + pricesArray[4]+pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[1] + pricesArray[4]+pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[1] + pricesArray[4]+pricesArray[8];
+        }
+      }else if(newOrder.crust == "St. Louis Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[1] + pricesArray[5]+pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[1] + pricesArray[5]+pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[1] + pricesArray[5]+pricesArray[8];
+        }
+      }
+    }else if( newOrder.size == "Large"){
+      if ( newOrder.crust == "Neapolitan Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[2] + pricesArray[3]+pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[2] + pricesArray[3]+pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[2] + pricesArray[3]+pricesArray[8];
+        }
+      }else if(newOrder.crust == "New York Style Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[2] + pricesArray[4]+pricesArray[6];
+        }else if(newOrder == "Sausage"){
+          var totalPrice = pricesArray[2] + pricesArray[4]+pricesArray[7];
+        }else if(newOrder == "Bacon"){
+          var totalPrice = pricesArray[2] + pricesArray[4]+pricesArray[8];
+        }
+      }else if(newOrder.crust == "St. Louis Pizza"){
+        if (newOrder.toppings == "Onions"){
+          var totalPrice = pricesArray[2] + pricesArray[5]+pricesArray[6];
+        }else if(newOrder == "Sausage"){
+           var totalPrice = pricesArray[2] + pricesArray[5] + pricesArray[7];
+        }else if(newOrder == "Bacon"){ 
+          alert("Hello");
+        }
+      }
+    }
+
+    // if( selectedSize == "Small" && selectedCrust == "Neapolitan Pizza" && selectedToppings == "Onions"){
+    //   var totalPrice = pricesArray[0] + pricesArray[3] +pricesArray[6];
+    // } else{
+    //   var totalPrice = "Hello";
+      
+    // }
     
-    $("ol#order").append("<li><span class='summary'>"+newOrder.crust+"  "+newOrder.toppings+" "+newOrder.size+ "</span></li>");
+    
+     $("ol#order").append("<li><span class='summary'>"+newOrder.crust+"  "+newOrder.toppings+" "+newOrder.size+" "+totalPrice+ "</span></li>");
+    //  $("ol#order").append("<li><span class='summary'>"+newOrder.crust+"  "+newOrder.toppings+" "+newOrder.size+" "+totalPrice2+ "</span></li>");
+    //  $("ol#order").append("<li><span class='summary'>"+newOrder.crust+"  "+newOrder.toppings+" "+newOrder.size+" "+totalPrice3+ "</span></li>");
    
   });
 });
